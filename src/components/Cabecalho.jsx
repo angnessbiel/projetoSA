151a0e389efc0dcba52738f09cae3e003b1d08d9
angnessbiel <131162584+angnessbiel@users.noreply.github.com>
2024-05-21 11:18:07 -0300

    commit cabecalho

--- src/components/Cabecalho.jsx
+++ src/components/Cabecalho.jsx
@@ -7,11 +7,8 @@ function Cabecalho(){
             </div> 
             <div className="nav">
               <button onclick="scrollToElement('Sobre')">Parceiros</button>  
-<<<<<<< HEAD
              <a href="https://www.figma.com/proto/J5BUXyEGJtHqBfcKtVJpjF/alta-fidelidade?node-id=0-23&t=hVTUin2MM6Cgc8J3-0&scaling=min-zoom&page-id=0%3A1&starting-point-node-id=0%3A187"><button onclick="scrollToElement('secao-alvo')" >Protótipo</button></a> 
-=======
                            <a href="https://www.figma.com/proto/J5BUXyEGJtHqBfcKtVJpjF/alta-fidelidade?node-id=0-23&t=hVTUin2MM6Cgc8J3-0&scaling=min-zoom&page-id=0%3A1&starting-point-node-id=0%3A187"><button onclick="scrollToElement('secao-alvo')" >Protótipo</button></a> 
->>>>>>> f3aaef8abc2223ab9ea1567cb2899254896d229f
               <button>Time</button> 
             </div>        
         </header>
